7435803bb5a789d6708ff98e343df0b4bdf96948
kajahl <kacperjahl37@gmail.com>
2025-08-15 20:44:33 +0200

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.json');
+import { pathsToModuleNameMapper } from 'ts-jest';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-module.exports = {
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const tsconfig = JSON.parse(fs.readFileSync(path.join(__dirname, 'tsconfig.json'), 'utf8'));
+
+export default {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+	moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions?.paths || {}, { prefix: '<rootDir>/' }),
 };
