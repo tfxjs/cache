e385f8536b91a2ac9fe7d008754b20df3d921836
kajahl <kacperjahl37@gmail.com>
2025-08-19 15:56:52 +0200

    feat: add timeout @ BasicFetcher - resolving values with delay

--- src/fetchers/BasicFetcher.ts
+++ src/fetchers/BasicFetcher.ts
@@ -1,10 +1,13 @@
 import { TFetchStrategy } from '@src/types';
 
 export default class BasicFetcher<T> implements TFetchStrategy<T> {
-	private values: Map<string, T | null> = new Map();
+	private readonly values: Map<string, T | null> = new Map();
+	private readonly defaultTimeout: number = 100;
+	private readonly timeoutMap: Map<string, number> = new Map();
 
-	setReturnValue(id: string, value: T | null): void {
+	setReturnValue(id: string, value: T | null, timeout: number = this.defaultTimeout): void {
 		this.values.set(id, value);
+		this.timeoutMap.set(id, timeout);
 	}
 
 	unsetReturnValue(id: string): void {
@@ -16,6 +19,11 @@ export default class BasicFetcher<T> implements TFetchStrategy<T> {
 	}
 
 	fetch(id: string): Promise<T | null> {
-		return Promise.resolve(this.values.get(id) || null);
+		return new Promise((resolve) => {
+			const timeout = this.timeoutMap.get(id) || this.defaultTimeout;
+			setTimeout(() => {
+				resolve(this.values.get(id) || null);
+			}, timeout);
+		});
 	}
 }
